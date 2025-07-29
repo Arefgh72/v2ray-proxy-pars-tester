c83e6a38f5ef70ba5316bda62833f2a26409d5d4
Arefgh72 <129980673+Arefgh72@users.noreply.github.com>
2025-07-29 05:47:33 +0330

    Update 02_test_hysteria.py

--- scripts/02_test_hysteria.py
+++ scripts/02_test_hysteria.py
@@ -10,7 +10,7 @@ import random
 import string
 
 # توابع کمکی را از فایل utils وارد می‌کنیم
-from utils import get_proxies_from_file, save_proxies_to_file, save_json_to_file
+from .utils import get_proxies_from_file, save_proxies_to_file, save_json_to_file
 
 # مسیر فایل اجرایی کلاینت Hysteria که در ورک‌فلو دانلود می‌شود
 HYSTERIA_CLIENT_PATH = "./hysteria-client"
